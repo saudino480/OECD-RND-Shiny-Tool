d2accfeed5b2f7f97faa41fa18ff762a8117da48
Sam <saudino480@gmail.com>
2019-06-21 04:23:13 -0400

    fixed label_maker error

--- helper.R
+++ helper.R
@@ -4,9 +4,9 @@
 label_maker = function(col1, col2) {
   temp1 = as.character(col1)
   temp2 = as.character(col2)
-  if (temp1 == "Percentage") {
+  if (temp1[1] == "Percentage") {
     return("Percentage (%)")
-  } else if (temp1 == "US Dollar") {
+  } else if (temp1[1] == "US Dollar") {
     return(paste(temp1, "in", temp2, sep=" "))
   } else {
     return("Error")
